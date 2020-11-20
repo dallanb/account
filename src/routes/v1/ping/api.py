81cee8b02580b1d6dda0bb7a72623260fee1f3fa
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-11-20 12:03:21 -0800

    set api back to what it was originally

--- src/routes/v1/ping/api.py
+++ src/routes/v1/ping/api.py
@@ -9,4 +9,4 @@ class PingAPI(Base):
 
     @marshal_with(MessageResponse.marshallable())
     def get(self):
-        return MessageResponse(message='pang')
+        return MessageResponse(message='pong')
