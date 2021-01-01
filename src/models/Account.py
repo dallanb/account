cb5fe9f5823cf280a98bd729d9b638a6f099c5d6
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-01 11:51:31 -0800

    gut the avatar and search components from account

--- src/models/Account.py
+++ src/models/Account.py
@@ -1,5 +1,4 @@
 from sqlalchemy_utils import EmailType, UUIDType
-from sqlalchemy_utils.types import TSVectorType
 
 from .mixins import BaseMixin
 from .. import db
@@ -12,22 +11,17 @@ class Account(db.Model, BaseMixin):
     username = db.Column(db.String(15), unique=True, nullable=False)
     display_name = db.Column(db.String(50), nullable=True)
 
-    # Search
-    search_vector = db.Column(TSVectorType('display_name', 'username', weights={'display_name': 'A', 'username': 'B'}))
-
     # FK
     status = db.Column(db.Enum(StatusEnum), db.ForeignKey('status.name'), nullable=False)
     role = db.Column(db.Enum(RoleEnum), db.ForeignKey('role.name'), nullable=False)
     address_uuid = db.Column(UUIDType(binary=False), db.ForeignKey('address.uuid'), nullable=True)
     phone_uuid = db.Column(UUIDType(binary=False), db.ForeignKey('phone.uuid'), nullable=True)
-    avatar_uuid = db.Column(UUIDType(binary=False), db.ForeignKey('avatar.uuid'), nullable=True)
 
     # Relationship
     account_status = db.relationship("Status")
     account_role = db.relationship("Role")
     address = db.relationship("Address", back_populates="account")
     phone = db.relationship("Phone", back_populates="account")
-    avatar = db.relationship("Avatar", back_populates="account")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
