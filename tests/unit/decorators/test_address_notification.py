aab68071b6f7b4c3edfe27b9ea61c2c88e3fe24d
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-08 16:56:46 -0700

    even more delay

--- tests/unit/decorators/test_address_notification.py
+++ tests/unit/decorators/test_address_notification.py
@@ -9,7 +9,7 @@ from src import services
 def test_address_notification_country_updated(reset_db, kafka_conn_last_msg, seed_account):
     address = services.AddressService().create(account=pytest.account, country=pytest.country)
     _ = services.AddressService().update(uuid=address.uuid, country='US')
-    time.sleep(0.5)
+    time.sleep(1)
     msg = kafka_conn_last_msg('accounts')
     assert msg.key is not None
     assert msg.key == 'country_updated'
